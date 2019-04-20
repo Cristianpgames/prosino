31c20ead45e1a866e4980ae3588188a3d0d519b5
Cristianpgames <cristian.arese@epiagranollers.cat>
2019-04-20 14:40:36 +0200

    Proyecto Base

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float velocityY = 0.5f;
+    public float velocityX = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(Vector2.up * velocityY, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(Vector2.right * velocityX, ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(Vector2.left * velocityX, ForceMode2D.Impulse);
+        }
+    }
+}
